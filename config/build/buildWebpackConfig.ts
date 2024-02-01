58081ecccafe5721c2c13f5a73ed64d2dad014b7
Djamalayl <tsutsaev@MacBook-Air.local>
2024-02-01 10:05:27 +0300

    config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,24 @@
+import path from "path";
+import { BuidOptions } from "./types/config";
+import webpack from 'webpack'
+import { buildPlugins } from "./buildPlugins";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolvers } from "./buildResolvers";
+
+export function buidWebpackConfig(options: BuidOptions): webpack.Configuration {
+    const {paths, mode} = options;
+    return {
+        mode,
+        entry: paths.entry,
+        output: {
+            filename:'[name][contanthash].js',
+            path: paths.build,
+            clean: true,
+        },
+        plugins:buildPlugins(options),
+        module: {
+            rules: buildLoaders()
+          },
+          resolve: buildResolvers(),
+      }
+}
