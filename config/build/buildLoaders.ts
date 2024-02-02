5498b5c21dd545aeaf41e6c8a02c5c50f19cbc54
Djamalayl <tsutsaev@MacBook-Air.local>
2024-02-02 17:08:19 +0300

    scss

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,21 +1,34 @@
-import webpack from 'webpack'
-export function buildLoaders(): webpack.RuleSetRule[] {
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
 
-    const cssLoader =  {
-      test: /\.s[ac]ss$/i,
-      use: [
-        // Creates `style` nodes from JS strings
-        "style-loader",
-        // Translates CSS into CommonJS
-        "css-loader",
-        // Compiles Sass to CSS
-        "sass-loader",
-      ],
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]'
+                    },
+                }
+            },
+            'sass-loader',
+        ]
     }
-    const typescriptLoader =  {
+
+    const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-      }
-    return [typescriptLoader, cssLoader]
+    }
+
+    return [
+        typescriptLoader,
+        cssLoader,
+    ]
 }
